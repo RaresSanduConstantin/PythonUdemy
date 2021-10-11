8e5f2535aad54e856018d2fe2e9b864774d3f4ca
Sandu <sandurares57@gmail.com>
2021-10-11 10:44:54 +0300

    monday py

---
+++ PythonBasics/28.py
@@ -0,0 +1,4 @@
+# augmentet assigment operator
+some_value = 5
+some_value += 2
+print(some_value)
--- PythonBasics/29.py
+++ PythonBasics/29.py
@@ -1,4 +1,21 @@
-
+print(type('asdadas'))
 iq = 190
 user_age = iq / 4
 print(user_age)
+
+username = 'supercoder'
+password = 'supersecret'
+long_string = '''w
+wow
+asdasd
+
+---
+'''
+
+print(long_string)
+
+first_name = 'Rares'
+last_name = 'Sandu'
+
+full_name = first_name + ' ' + last_name
+print(full_name)
